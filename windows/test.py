6659baf7d88f72b2222172ad74f7865498cf5ac5
NiklasHubGit <hellovvorld@tuta.io>
2023-03-18 11:41:39 +0100

    Fast feddig

--- windows/test.py
+++ windows/test.py
@@ -1,25 +1,19 @@
 from plyer import notification
 from io import BytesIO
 import subprocess
-
-# for simple commands
-#subprocess.run(["ls", "-l"]) 
-#subprocess.run(["echo "jldss"']) 
-
 import os 
-  
-# Command to execute
-# Using Windows OS command
+hw3Limit = 1000
 curl = 'curl.exe -G "https://eu1.cloud.thethings.network/api/v3/as/applications/temp-sens-jgo/packages/storage/uplink_message" -H  "Authorization: Bearer NNSXS.WBQ36NNFPGABFN4I6WAOLC6JNGT66ALKRIA2HWY.UHGG3ETMOJWGN6AJMAH63B6HNG5TAUCW2ETETKUN4T3PG7HAWADA" > alles.txt'
 os.system(curl)
+
 awk = 'script.bat'
 os.system(awk)
 Distance = os.popen("type Distance.txt").read();
-#print("Distance")
 print(Distance)
 
-notification.notify(
-    title = "ALERT!!!",
-    message = "Achtung bei dir gibts hochwasser",
-    timeout = 1
-)
+if hw3Limit > int(Distance):
+    notification.notify(
+        title = "ALERT!!!",
+        message = "Achtung bei dir gibts hochwasser",
+        timeout = 1
+    )
