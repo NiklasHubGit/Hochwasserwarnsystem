05ae38740ad3ea4e5b92810994dd2d9b14fe7f16
NiklasHubGit <hellovvorld@tuta.io>
2023-03-18 11:57:01 +0100

    Bugfix

--- windows/script.bat
+++ windows/script.bat
@@ -1,3 +1,3 @@
 awk.exe "END{print}" alles.txt > letzte.txt
 awk.exe -F "," "{print $9}" letzte.txt > .temp.txt
-awk.exe -F ":" "{print $3}" .temp.txt > Distance
+awk.exe -F ":" "{print $3}" .temp.txt > Distance.txt
--- windows/test.py
+++ windows/test.py
@@ -2,7 +2,7 @@ from plyer import notification
 from io import BytesIO
 import subprocess
 import os 
-hw3Limit = 1100
+hw3Limit = 1000
 curl = 'curl.exe -G "https://eu1.cloud.thethings.network/api/v3/as/applications/temp-sens-jgo/packages/storage/uplink_message" -H  "Authorization: Bearer NNSXS.WBQ36NNFPGABFN4I6WAOLC6JNGT66ALKRIA2HWY.UHGG3ETMOJWGN6AJMAH63B6HNG5TAUCW2ETETKUN4T3PG7HAWADA" > alles.txt'
 os.system(curl)
 
