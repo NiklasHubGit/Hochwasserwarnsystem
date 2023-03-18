6d298eeb79e412dc01fee5f07e2b663f83d145a9
NiklasHubGit <hellovvorld@tuta.io>
2023-03-18 11:54:49 +0100

    funktioniert jetzt auch in anderen home dirs

--- windows/script.bat
+++ windows/script.bat
@@ -1,3 +1,3 @@
-awk.exe.lnk "END{print}" C:\Users\niklas-win11\Documents\GitHub\Hochwasserwarnsystem\windows\alles.txt > letzte.txt
-awk.exe.lnk -F "," "{print $9}" C:\Users\niklas-win11\Documents\GitHub\Hochwasserwarnsystem\windows\letzte.txt > .temp.txt
-awk.exe.lnk -F ":" "{print $3}" C:\Users\niklas-win11\Documents\GitHub\Hochwasserwarnsystem\windows\.temp.txt > Distance.txt
+awk.exe "END{print}" alles.txt > letzte.txt
+awk.exe -F "," "{print $9}" letzte.txt > .temp.txt
+awk.exe -F ":" "{print $3}" .temp.txt > Distance
