fb5ace21de02e258f71691cd6960878757d08e6a
NiklasHubGit <128160948+NiklasHubGit@users.noreply.github.com>
2023-03-23 20:19:53 +0100

    llllooppp
    while true= false

--- windows/test.py
+++ windows/test.py
@@ -1,19 +1,21 @@
+from time import sleep
 from plyer import notification
-from io import BytesIO
 import subprocess
 import os 
-hw3Limit = 1000
+hw3Limit = 3000
 curl = 'curl.exe -G "https://eu1.cloud.thethings.network/api/v3/as/applications/temp-sens-jgo/packages/storage/uplink_message" -H  "Authorization: Bearer NNSXS.WBQ36NNFPGABFN4I6WAOLC6JNGT66ALKRIA2HWY.UHGG3ETMOJWGN6AJMAH63B6HNG5TAUCW2ETETKUN4T3PG7HAWADA" > alles.txt'
-os.system(curl)
+while True:
+    os.system(curl)
+    awk = 'script.bat'
+    os.system(awk)
+    Distance = os.popen("type Distance.txt").read();
+    print(Distance)
 
-awk = 'script.bat'
-os.system(awk)
-Distance = os.popen("type Distance.txt").read();
-print(Distance)
-
-if hw3Limit > int(Distance):
-    notification.notify(
-        title = "ALERT!!!",
-        message = "Achtung bei dir gibts hochwasser",
-        timeout = 1
-    )
+    if hw3Limit > int(Distance):
+        notification.notify(
+            title = "ALERT!!!",
+            message = "Achtung bei dir gibts hochwasser",
+            timeout = 4
+        )
+    sleep(30)
+    continue
