0aecd51a0bcdad03ed811a82c0284394731101ef
NiklasHubGit <128160948+NiklasHubGit@users.noreply.github.com>
2023-03-31 09:40:47 +0000

    Update test.py

--- windows/test.py
+++ windows/test.py
@@ -2,7 +2,7 @@ from time import sleep
 from plyer import notification
 import subprocess
 import os 
-hw3Limit = 3000
+Limit = 1000
 curl = 'curl.exe -G "https://eu1.cloud.thethings.network/api/v3/as/applications/temp-sens-jgo/packages/storage/uplink_message" -H  "Authorization: Bearer NNSXS.WBQ36NNFPGABFN4I6WAOLC6JNGT66ALKRIA2HWY.UHGG3ETMOJWGN6AJMAH63B6HNG5TAUCW2ETETKUN4T3PG7HAWADA" > alles.txt'
 while True:
     os.system(curl)
