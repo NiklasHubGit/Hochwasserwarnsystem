93e96524d9e575dafd937d0583c7ab47b7d682af
NiklasHubGit <128160948+NiklasHubGit@users.noreply.github.com>
2023-04-17 21:08:34 +0200

    Update main.dart

--- flutter2/lib/main.dart
+++ flutter2/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: const RootPage(),
         theme: ThemeData(
-          primarySwatch: Colors.purple,
+          primarySwatch: Colors.blue,
         ));
   }
 }
@@ -24,6 +26,22 @@ class RootPage extends StatefulWidget {
   State<RootPage> createState() => _RootPageState();
 }
 
+List standorte = [
+  "Ochsenhausen",
+  "Biberach",
+  "Aulendorf",
+];
+Map messstationen = ({
+  "Ochsenhausen": "4",
+  "Biberach": "3",
+  "Aulendorf": "2",
+});
+Map warnung = ({
+  "Ochsenhausen": "keine Warnung",
+  "Biberach": "keine Warnung",
+  "Aulendorf": "keine Warnung",
+});
+
 class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
@@ -34,36 +52,24 @@ class _RootPageState extends State<RootPage> {
         body: Center(
             // child: Text('Hello World'),
             child: Column(children: [
-          Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 193, 115, 207),
-                border: Border.all(
-                    color: Color.fromARGB(255, 19, 17, 17), width: 2),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: const [
-                  Text('Ochsenhausen'),
-                  Text('keine Warnung'),
-                  Text('4 Messstationen'),
-                ],
-              )),
-          Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 193, 115, 207),
-                border: Border.all(
-                    color: Color.fromARGB(255, 19, 17, 17), width: 2),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: const [
-                  Text('Ochsenhausen'),
-                  Text('keine Warnung'),
-                  Text('4 Messstationen'),
-                ],
-              ))
+          for (var i = 0; i < 3; i++)
+            Container(
+                padding: const EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                width: 999999999999,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 34, 110, 197),
+                  border: Border.all(
+                      color: Color.fromARGB(255, 19, 17, 17), width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(standorte[i]),
+                    Text("Messtationen " + (messstationen[standorte[i]])),
+                    Text(warnung[standorte[i]]),
+                  ],
+                )),
         ])));
   }
 }
